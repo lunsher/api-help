2d54a41b2708a8ab5786a3be314187a1f75a7e13
Evgeniy Solovyov <lunsher@gmail.com>
2011-10-19 22:53:43 +0300

    created rails app

---
+++ apihelp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apihelp::Application.config.secret_token = '015dee36175ace3ffff51fa59500ae2bf21cc160413297672ecb50c71a12398a8a022077c6b06ca56c4d29f912d0f581fe66e095c8ac0e4beb62bc8bfff6b960'
